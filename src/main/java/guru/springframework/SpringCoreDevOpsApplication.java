8b62c50d045ab7d54c7778d7d0f4cd66399c442f
Vladimir Ovcharov <vladimir.1527@gmail.com>
2018-03-18 18:49:20 +0200

    Testing CI build 2

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -8,5 +8,7 @@ public class SpringCoreDevOpsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
+
+		//test for CI build
 	}
 }
